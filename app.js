3e792fa139f452dbd0c83ed185b5a75adafdebcf
D Kevin <kevindemaria@gmail.com>
2021-09-07 11:22:16 +0530

    remove some extra space

--- app.js
+++ app.js
@@ -5,30 +5,22 @@ const dotenv = require("dotenv");
 
 // creating a express app
 const app = express();
-
 // middleware for cors(cross origin resource sharing)
 app.use(cors());
-
 // middleware for json
 app.use(express.json());
-
 // middleware for urlextended
 app.use(express.urlencoded({ extended: true }));
-
 // calling the database function
 databaseConnection();
-
 // configuring the dotenv
 dotenv.config();
-
 // Creating a PORT
 const PORT = process.env.PORT;
 
 //listening to the incoming request
 app.listen(PORT, (err) => {
-
     if (err) return console.log(err.message);
-
     console.log(`Server Started Listening at PORT ${PORT}`);
 });
 
