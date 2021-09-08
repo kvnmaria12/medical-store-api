4415bdcba3eb41322e984a8d6ab948c6fffa07a6
D Kevin <kevindemaria@gmail.com>
2021-09-08 16:18:04 +0530

    add routes

---
+++ routes/medicine.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const medicineController = require("../controllers/deleteMedicineController");
+
+// creating a router object
+const router = express.Router();
+// creating a route
+router.post("/delete", medicineController.deleteMedicineData);
+// exporting the router object
+module.exports = router;
+
