59c0913f7a97f7ccea6a88dec16d567a6a793b8c
D Kevin <kevindemaria@gmail.com>
2021-09-09 10:23:17 +0530

    change a route path

--- routes/medicine.js
+++ routes/medicine.js
@@ -3,6 +3,6 @@ const medicineController = require("../controller/createMedicineController");
 // creating a router object
 const router = express.Router();
 // creating a route called create
-router.post("/create", medicineController.createMedicine);
+router.post("/", medicineController.createMedicine);
 // exporting the module
 module.exports = router;
