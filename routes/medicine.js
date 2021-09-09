938660efcb9da1a1feed3033e8c491fdcb9f370f
D Kevin <kevindemaria@gmail.com>
2021-09-09 16:14:24 +0530

    added an extra path within a single route by passing an array of endpoints

--- routes/medicine.js
+++ routes/medicine.js
@@ -4,8 +4,6 @@ const medicineController = require("../controllers/fetchMedicineController");
 // creating a router object
 const router = express.Router();
 // creating a route path for passing a params id
-router.get("/:id", medicineController.fetchMedicineDataWithId);
-// creating a route path to display all the data
-router.get("/", medicineController.fetchAllMedicineData);
+router.get(["/:id", "/"], medicineController.fetchMedicineDataWithId);
 // exporting the router object
 module.exports = router;
