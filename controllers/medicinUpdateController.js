4c1468aae8137aace2c4172bd2f6b454db75b2cc
D Kevin <kevindemaria@gmail.com>
2021-09-09 15:31:06 +0530

    add controllers

---
+++ controllers/medicinUpdateController.js
@@ -0,0 +1,10 @@
+const apiResponse = require("../helpers/apiResponse");
+const { con } = require("../database");
+
+exports.updateMedicineData =
+    [
+        // route handler 
+        (req, res) => {
+
+        }
+    ]
