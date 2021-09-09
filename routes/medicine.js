5c68e339545f2e6a07b61abbd1fcc18d0ffff932
D Kevin <kevindemaria@gmail.com>
2021-09-09 10:16:12 +0530

    add a extra router and change the verb to GET

--- routes/medicine.js
+++ routes/medicine.js
@@ -3,7 +3,9 @@ const medicineController = require("../controllers/fetchMedicineController");
 
 // creating a router object
 const router = express.Router();
-// creating a route
-router.post("/data", medicineController.fetchMedicineData);
+// creating a route path for passing a params id
+router.get("/:id", medicineController.fetchMedicineDataWithId);
+// creating a route path to display all the data
+router.get("/", medicineController.fetchAllMedicineData);
 // exporting the router object
 module.exports = router;
