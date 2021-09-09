7eb32126968c96c0f53923ea4f959ca606d9de23
D Kevin <kevindemaria@gmail.com>
2021-09-09 10:28:39 +0530

    change the route path

--- routes/medicine.js
+++ routes/medicine.js
@@ -4,7 +4,7 @@ const medicineController = require("../controllers/deleteMedicineController");
 // creating a router object
 const router = express.Router();
 // creating a route
-router.post("/delete", medicineController.deleteMedicineData);
+router.post("/:id", medicineController.deleteMedicineData);
 // exporting the router object
 module.exports = router;
 
