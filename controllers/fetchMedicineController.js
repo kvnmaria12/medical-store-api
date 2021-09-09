de3533db6821229914fe3bea10260c536d2a6eda
D Kevin <kevindemaria@gmail.com>
2021-09-09 16:14:37 +0530

    add some code to get the query data

--- controllers/fetchMedicineController.js
+++ controllers/fetchMedicineController.js
@@ -6,41 +6,52 @@ exports.fetchMedicineDataWithId =
     [
         // router handler function
         (req, res) => {
-
             try {
+                // if the client passes a parameter
+                const query = req.query;
+                // get the length of the key values of the query object
+                const queryLength = Object.keys(query).length;
+                // id passed by the client as a parameter
                 const id = req.params.id;
-                // SQL Query for fetching the data from the database
-                const sqlQuery = `SELECT * FROM medicine WHERE id = '${id}'`;
-                // getting the medicine data from the mysql database
-                con.query(sqlQuery, (err, dbData) => {
-                    // if error return an apiResponse as serverSide Error
-                    if (err) return apiResponse.serverSideError(res, "Server Side Error");
-                    return apiResponse.successResponseWithData(res, "Success", dbData);
-                })
-            } catch (error) {
-                return apiResponse.serverSideError(res, "Server Side Error");
-            }
-        }
-    ];
+                // if the client passes a id
+                if (id) {
+                    // SQL Query for fetching the data from the database
+                    const sqlQuery = `SELECT * FROM medicine WHERE id = '${id}'`;
+                    // getting the medicine data from the mysql database
+                    con.query(sqlQuery, (err, dbData) => {
+                        // if error return an apiResponse as serverSide Error
+                        if (err) return apiResponse.serverSideError(res, "Server Side Error");
+                        // otherwise return the database data to the client
+                        return apiResponse.successResponseWithData(res, "Success", dbData);
+                    });
 
-// To fetch all the data from the database
-exports.fetchAllMedicineData =
-    [
-        // route handler function
-        (req, res) => {
+                } else if (queryLength > 0) {  // if the queryLength is greater than 0
 
-            try {
-                // SQL Query for fetching all the data from the database
-                const sqlQuery = `SELECT * FROM medicine`;
-                // getting the medicine data from the mysql database
-                con.query(sqlQuery, (err, dbData) => {
-                    // if error return an apiResponse as serverSide Error
-                    if (err) return apiResponse.serverSideError(res, "Server Side Error");
-                    // returning a success response
-                    return apiResponse.successResponseWithData(res, "Success", dbData);
-                })
+                    // to get the query data 
+                    const { Name: MedicineName } = query;
+                    // SQL Query for fetching the data from the database`
+                    const sqlQuery = `SELECT * FROM medicine WHERE name = '${MedicineName}'`
+                    // getting the medicine data based on the parameter
+                    con.query(sqlQuery, (err, dbData) => {
+                        // if error return an apiResponse as a serverSide Error
+                        if (err) return apiResponse.serverSideError(res, "Server Side Error");
+                        // otherwise return the database data to the client
+                        return apiResponse.successResponseWithData(res, "Success", dbData);
+                    });
+                } else { //if the client doest not pass anything then send the whole db data
+
+                    // SQL Query for fetching all the data from the database
+                    const sqlQuery = `SELECT * FROM medicine`;
+                    // getting the medicine data from the mysql database
+                    con.query(sqlQuery, (err, dbData) => {
+                        // if error return an apiResponse as serverSide Error
+                        if (err) return apiResponse.serverSideError(res, "Server Side Error");
+                        // returning a success response
+                        return apiResponse.successResponseWithData(res, "Success", dbData);
+                    })
+                }
             } catch (error) {
-                // returning a server side error response
+                // if any server side error return an response to the client
                 return apiResponse.serverSideError(res, "Server Side Error");
             }
         }
