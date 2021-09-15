94630da3605f4ae466ba1d9212c3e8fbf238aef6
D Kevin <kevindemaria@gmail.com>
2021-09-15 13:15:57 +0530

    add some middlewares

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const apiResponse = require("./helpers/apiResponse");
+const medicine = require("./routes/api");
 const { databaseConnection } = require("./database");
 
 // creating a instance of express app
@@ -17,15 +19,23 @@ dotenv.config();
 databaseConnection();
 
 // middleware for the medicine route
-// app.use("/api",)
-
+app.use("/api", medicine);
+// Not Found response for unknown endpoint
+app.all("*", (req, res) => {
+    return apiResponse.notFoundResponse(res, "Not Found");
+});
+// middleware to check for each and every request for unauthorizedError
+app.use((err, req, res) => {
+    if (err.name === 'UnauthorizedError') {
+        return apiResponse.unauthorizedError(res, "UnauthorizedError");
+    }
+});
 
 // creating a PORT
 const PORT = process.env.PORT || 1212;
-
 // listening to all the incoming request
 app.listen(PORT, (err) => {
-    // if error return 
+    // if error return the error message 
     if (err) return console.log(err.message);
     console.log(`Server Started Listening at PORT ${PORT}`);
 });
