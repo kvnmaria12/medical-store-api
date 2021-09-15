7da89fca245f76b3a418308a2672df6715f3553d
D Kevin <kevindemaria@gmail.com>
2021-09-15 13:18:03 +0530

    some little changes

--- routes/medicine.js
+++ routes/medicine.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const medicineController = require("../controllers/medicineUpdateController");
 
 // creating a router object
 const router = express.Router();
 //creating a route path
-router.post("/",);
+router.put("/:id", medicineController.updateMedicineData);
 // exporting the router object
 module.exports = router;
