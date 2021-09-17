5307bd7c0fbf3b5f4e65d7a4c49f09c1caebca3f
D Kevin <kevindemaria@gmail.com>
2021-09-18 00:00:51 +0530

    add some changes

--- controller/createMedicineController.js
+++ controller/createMedicineController.js
@@ -6,24 +6,24 @@ const { validationResult, check } = require("express-validator");
 // exporting the createMedicine function
 exports.createMedicine =
     [
-        // checking the request body with certain conditions using express-validator
-        check("id").notEmpty().withMessage("Medicine Id cannot be blank").isNumeric().withMessage("Medicine id must be a number"),
-        check("name").notEmpty().withMessage("Medicine Name cannot be blank").isLength({ min: 2 }).withMessage("Medicine Name must have atleast 4 characters"),
-        check("price").notEmpty().withMessage("Medicine Price cannot be blank").isNumeric().withMessage("Medicine Price must be a number").isLength({ min: 2 }).withMessage("Medicine Price must have atleast 2 characters"),
-        check("quantity").notEmpty().withMessage("Medicine Quantity cannot be blank").isNumeric().withMessage("Medicine quantity must be a number").isLength({ min: 2 }).withMessage("Medicine Quantity must have atleast 2 characters"),
-        check("categories").notEmpty().withMessage("Medicine Quantity cannot be blank").isLength({ min: 2 }).withMessage("Medicine Categories must have atleast 2 characters"),
-        check("manufacture_date").notEmpty().withMessage("Manufactured_Date cannot be blank").isLength({ min: 2 }).withMessage("Manufactured_Date must have atleast 2 characters"),
-        check("expiry_date").notEmpty().withMessage("Expiry_Date cannot be blank").isLength({ min: 2 }).withMessage("Expiry_Date must have atleast 2 characters"),
+        // // checking the request body with certain conditions using express-validator
+        // check("id").notEmpty().withMessage("Medicine Id cannot be blank").isNumeric().withMessage("Medicine id must be a number"),
+        // check("name").notEmpty().withMessage("Medicine Name cannot be blank").isLength({ min: 2 }).withMessage("Medicine Name must have atleast 4 characters"),
+        // check("price").notEmpty().withMessage("Medicine Price cannot be blank").isNumeric().withMessage("Medicine Price must be a number").isLength({ min: 2 }).withMessage("Medicine Price must have atleast 2 characters"),
+        // check("quantity").notEmpty().withMessage("Medicine Quantity cannot be blank").isNumeric().withMessage("Medicine quantity must be a number").isLength({ min: 2 }).withMessage("Medicine Quantity must have atleast 2 characters"),
+        // check("categories").notEmpty().withMessage("Medicine Quantity cannot be blank").isLength({ min: 2 }).withMessage("Medicine Categories must have atleast 2 characters"),
+        // check("manufacture_date").notEmpty().withMessage("Manufactured_Date cannot be blank").isLength({ min: 2 }).withMessage("Manufactured_Date must have atleast 2 characters"),
+        // check("expiry_date").notEmpty().withMessage("Expiry_Date cannot be blank").isLength({ min: 2 }).withMessage("Expiry_Date must have atleast 2 characters"),
 
         // once the express-validator checks for the above condition then we come the route handler function
         (req, res) => {
             try {
-                // the validationResult function checks for all the errors in the req.body 
-                const errors = validationResult(req);
-                // if errors are there then
-                if (!errors.isEmpty()) {
-                    return apiResponse.clientSideErrorWithData(res, "Client Side Error", { Message: errors.array() });
-                }
+                // // the validationResult function checks for all the errors in the req.body 
+                // const errors = validationResult(req);
+                // // if errors are there then
+                // if (!errors.isEmpty()) {
+                //     return apiResponse.clientSideErrorWithData(res, "Client Side Error", { Message: errors.array() });
+                // }
 
                 const id = req.body.id;
                 const name = req.body.name;
@@ -42,14 +42,16 @@ exports.createMedicine =
                 // query for inserting data in the database
                 con.query(sqlQuery, [values], (err) => {
                     // if err return server side error 
-                    if (err) return apiResponse.serverSideError(res, "Server Side Error");
+                    // if (err) return apiResponse.serverSideError(res, "Server Side Error");
+                    if (err) return console.log(err.message)
                     // returning a success response
                     return apiResponse.successResponse(res, "Date Entered Successfully");
                 });
                 // to catch the error
             } catch (error) {
                 // returning a server side error
-                return apiResponse.serverSideError(res, "Server Side Error");
+                // return apiResponse.serverSideError(res, "Server Side Error");
+                console.log(error)
             }
         }
     ];
