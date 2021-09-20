835bf9112fac6244b216b14f597e4dfb460599c5
D.Kevin <kevindemaria95@gmail.com>
2021-09-20 15:39:18 +0530

    add all files'

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const medicine = require("./routes/api");
+const apiResponse = require("./helpers/apiResponse");
+const { databaseConnection, con } = require("./database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+databaseConnection();
+
+app.use("/api", medicine);
+
+// Not Found response from the unknown endpoint
+app.all("*", (req, res) => {
+    return apiResponse.notFoundResponse(res, "Page Not Found");
+});
+
+// Creating a middleware for every request to check for unauthorizedError
+app.use((err, req, res) => {
+    if (err.name === "UnauthorizedError") {
+        return apiResponse.unauthorizedError(res, err.message);
+    }
+})
+
+const PORT = process.env.PORT || 1212;
+app.listen(PORT, (err) => {
+    if (err) return console.log(err.message);
+    console.log(`Server Started Listening at PORT ${PORT}`);
+});
+
+
