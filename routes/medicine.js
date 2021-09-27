8bd6d54951ecaebca766cecd6984ba4788f25fc9
D Kevin <kevindemaria95@gmail.com>
2021-09-27 13:58:44 +0530

    remove some whitespace

--- routes/medicine.js
+++ routes/medicine.js
@@ -1,20 +1,13 @@
 const express = require("express");
-
 const medicineController = require("../controllers/updateMedicineController");
-
-const router = express.Router();
-router.put("/:id", medicineController.updateMedicineDataWithId);
-=======
 const medicineController = require("../controllers/deleteMedicineController");
-
-const router = express.Router();
-router.delete("/:id", medicineController.deleteMedicineDataWithId);
-
 const medicineController = require("../controllers/createMedicineController");
 
 const router = express.Router();
+router.put("/:id", medicineController.updateMedicineDataWithId);
+const router = express.Router();
+router.delete("/:id", medicineController.deleteMedicineDataWithId);
+const router = express.Router();
 router.post("/", medicineController.createMedicineDataWithId);
-
-
 
 module.exports = router;
