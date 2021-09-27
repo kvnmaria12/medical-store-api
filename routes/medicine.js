f38a654c3049e31ab2c85f9b845bf083ce1c968d
D Kevin <kevindemaria95@gmail.com>
2021-09-27 14:02:33 +0530

    add route for fetching data

--- routes/medicine.js
+++ routes/medicine.js
@@ -2,7 +2,10 @@ const express = require("express");
 const medicineController = require("../controllers/updateMedicineController");
 const medicineController = require("../controllers/deleteMedicineController");
 const medicineController = require("../controllers/createMedicineController");
+const medicineController = require("../controllers/fetchMedicineController");
 
+const router = express.Router();
+router.get(["/:id", "/"], medicineController.fetchMedicineDataWithId);
 const router = express.Router();
 router.put("/:id", medicineController.updateMedicineDataWithId);
 const router = express.Router();
